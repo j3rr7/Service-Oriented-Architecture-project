0f0350f2471b74082051680e952f014a3d15c93f
Jeremy <adgrygry12348@gmail.com>
2021-05-05 12:03:41 +0700

    Update #9
    - add apiKey for user
    + update user page

--- user.js
+++ user.js
@@ -4,10 +4,19 @@ const router = express.Router();
 const db = require('./database');
 
 // Home page user route.
-router.get('/',  (req, res) => {
+router.get('/',  async (req, res) => {
     res.setHeader('Content-Type', 'text/html')
     if (req.session.currentUser) {
-        res.render("pages/user-page", { user : req.session.currentUser.data })
+
+        // GET USER DATA FROM DATABASE
+        let connection = await db.connection();
+        let query_user = await db.executeQuery(connection,
+            `SELECT * FROM users WHERE users.id = :id`,
+            { id : req.session.currentUser.data.id });
+        await db.release(connection);
+
+        // RENDER USER PAGE , with user params as identifier
+        res.render("pages/user-page", { user : query_user[0] })
         //res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
         //res.end('user signed in')
     } else {

48c506855a72cb0c4f97b7288bc59c243f64069b
Jeremy <adgrygry12348@gmail.com>
2021-05-15 12:19:09 +0700

    no tag
    just add something please ignore

--- api.js
+++ api.js
@@ -3,6 +3,16 @@ const router = express.Router();
 
 const db = require('./database');
 
+// Works as ENUM in Visual Studio (workaround for js)
+const ERROR_REASON = Object.freeze({
+    MissingKeyError         : -1,
+    InvalidKeyError         : 0,
+    ExpiredKeyError         : 1,
+    BillingNotEnabledError  : 2,
+    ApiNotActivatedError    : 3,
+    OverQuotaError          : 4
+})
+
 // ToDo : Seperate File for utils function (- maybe not needed)
 function GenerateApiKey( length ) {
     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -15,22 +25,39 @@ function GenerateApiKey( length ) {
     }).join('');
 }
 
+/*
+<summary>
+    MIDDLEWARE FOR FETCHING USER API-KEY FROM HEADER
+    if there's no x-api-key found in header return JSON object contains status error and status message
+    if x-api-key is found -> get user data based on provided x-api-key
+</summary>
+ */
+async function Middleware_APIKEY_FETCH( req, res, next ) {
+    // x-api-key is missing return error message
+    if (!req.header("x-api-key")) {
+        return res.status(401).send({
+            status  : res.statusCode,
+            //reason  : ERROR_REASON.MissingKeyError, /* OPTIONAL using enum to explain and show error message instead of hard coded string */
+            message : "Missing API-KEY"
+        });
+    }
+}
+
 // Home page api route.
 router.get('/', (req, res) => {
     res.status(403).send('API HOME PAGE');
-})
+});
 
 // About page api route.
 router.get('/about',  (req, res) => {
     res.status(403).send('API ABOUT PAGE');
-})
+});
 
 // SIGN UP
 router.post('/signup', async (req, res) => {
     let username    = req.body.username;
     let email       = req.body.email;
     let password    = req.body.password;
-    
     let connection = await db.connection();
 
     let query_username = await db.executeQuery(connection,
@@ -73,7 +100,7 @@ router.post('/signup', async (req, res) => {
     finally {
         await db.release(connection);
     }
-})
+});
 
 // SIGN IN
 router.post('/signin', async (req, res) => {
@@ -88,7 +115,7 @@ router.post('/signin', async (req, res) => {
 
     if (query_user.length < 1) {
         await db.release(connection);
-        return res.status(404).json({ status : 404 , message: "User Not Found" });
+        return res.status(404).json({ status : res.statusCode , message: "User Not Found" });
     }
 
     await db.release(connection);
@@ -111,31 +138,49 @@ router.post('/signin', async (req, res) => {
     }
 
     return res.status(400).json({ status : 400 , message: "Wrong Password"});
-})
+});
 
 // SIGN OUT
 router.post('/signout', async (req, res) => {
     if (req.session.currentUser) {
         req.session.destroy();
-        return res.status(200).json({ status : 200 , message: "User Logged Out"});
+        return res.status(200).json({ status : res.statusCode , message: "User Logged Out"});
     }
-    return res.status(400).json({ status : 400 , message: "User not signed in"});
-})
-
-// MIDDLEWARE GET USER API KEY
+    return res.status(400).json({ status : res.statusCode , message: "User not signed in"});
+});
 
 // GET POKEMON
 router.get('/pokemon', (req, res) => {
 
+});
+
+//#region BATTLE API
+//region BATTLE API
+/*
+    BATTLE
+    <summary>
+        Each battle will be assigned unique battle id at first
+        Each battle is unique to p1-pokemon vs p2-pokemon as it is 1vs1 pokemon battle
+        User must pass API-KEY(header), p1-pokemon, p2-pokemon
+        the function will take those API-KEY and compare that to system-generated API-KEY in database
+        Each turn will be determined based on returned JSON object
+        Battle will have its own status whether its ongoing, finished or dropped
+    </summary>
+ */
+
+// start of battle, history battle will be recorded based on this
+router.post('/battle', Middleware_APIKEY_FETCH, async (req, res) => {
+
 })
 
+//endregion
+//#endregion
 
-
-
+//#region MULTER API
+//region MULTER API
 /*
     MULTER SECTION
  */
-
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: function(req,file,callback){
@@ -199,5 +244,7 @@ router.post('/profile/upload', (req, res) => {
         return res.status(401).send('Subscription not found');
     }
 })
+//endregion
+//#endregion
 
 module.exports = router;
